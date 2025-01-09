a9646e50a2336535a59cdc229ad2e320fbe8b5b0
yassin-js <yrtal99@gmail.com>
2025-01-09 13:17:02 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,9 +6,8 @@ const Footer = () => {
       <footer className="mb-0 text-center">
         <div className="d-flex align-items-center justify-content-center pb-5">
           <div className="col-md-6">
-            <a className="text-dark fs-4" href="https://github.com/ssahibsingh" target="_blank" rel="noreferrer">
-              <i className="fa fa-github"></i>
-            </a>
+         
+            
           </div>
         </div>
       </footer>
